1a36ae1ade06afea5940af2c2e98a61830bfb4df
SadiqKhan-Dev <saiqkhan7777@gmail.com>
2025-11-21 18:18:34 +0500

    new update

--- students/SMIT-419473/sadiqkhan.js
+++ students/SMIT-419473/sadiqkhan.js
@@ -1,1 +1,22 @@
-JavaScript
+document.addEventListener("DOMContentLoaded", function () {
+
+    const buttons = document.querySelectorAll(".tab-btn");
+    const tabs = document.querySelectorAll(".tab-content");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+
+            // remove active classes
+            buttons.forEach(btn => btn.classList.remove("active"));
+            tabs.forEach(tab => tab.classList.remove("active"));
+
+            // add active class to clicked button
+            button.classList.add("active");
+
+            // show matching tab
+            const id = button.getAttribute("data-tab");
+            document.getElementById(id).classList.add("active");
+        });
+    });
+
+});
